d16c37b285ef281347bcb6d5b059006ee51f36f3
Marek Czyż <Marek.Czyz@dreamlab.pl>
2020-02-17 19:18:49 +0100

    GA added

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,33 @@ module.exports = {
     'MarkdownRemark.frontmatter.author': 'AuthorsYaml'
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'UA-114127986-2',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+        // Enables Google Optimize Experiment ID
+        experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'auto'
+      }
+    },
     // Adding various source folders to the GraphQL layer.
     {
       resolve: `gatsby-source-filesystem`,

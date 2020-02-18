30e75f3a8426ae5ae4e77fe13c36a9f1cdab7e23
Marek Czyż <Marek.Czyz@dreamlab.pl>
2020-02-18 19:27:41 +0100

    Other env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: process.env.GA_TRACKING_ID,
+        trackingId: process.env.GOOGLE_TRACKING_ID || 'none',
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
@@ -25,11 +25,11 @@ module.exports = {
         // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
         // Enables Google Optimize using your container Id
-        optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+        optimizeId: process.env.GOOGLE_OPTIMIZE_TRACKING_ID || 'none',
         // Enables Google Optimize Experiment ID
-        experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
+        experimentId: process.env.GOOGLE_EXPERIMENT_ID || 'none',
         // Set Variation ID. 0 for original 1,2,3....
-        variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
+        variationId: process.env.GOOGLE_OPTIMIZE_VARIATION_ID || 'none',
         // Any additional optional fields
         sampleRate: 5,
         siteSpeedSampleRate: 10,
